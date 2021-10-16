876f91aba19c79391a4e2a5a7faa7ec598082efe
pc <pc@pc-PC>
2021-10-16 11:49:31 -0700

    Selenium POM framework

---
+++ src/main/java/utilities/ConstantValue.java
@@ -0,0 +1,9 @@
+package utilities;
+
+public class ConstantValue {
+	public static final String userName="pallavithombre125@gmail.com";
+	public static final String password="Pallavi@125";
+	//public static final String userName="anshulc55";
+	//public static final String password="Test@1234";
+
+}
